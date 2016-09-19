d015ca047e1d8b7e2757e80d1f49317937858f7d
Petr Petrenko <ppetrenko@xiag.ch>
2016-09-19 18:51:31 +0700

    services

--- src/app.php
+++ src/app.php
@@ -4,9 +4,9 @@ $app = new Silex\Application();
 $app['debug'] = 1;
 $app->register(new \Janus\Silex\Provider\SystemConfigProvider(__DIR__.'/../etc/config.yml'));
 $app->register(new \Janus\Silex\Provider\DataBaseServiceProvider());
-$app->get('/getProxy', function () use ($app) {
+$app->get('/{service}', function ($service) use ($app) {
     $chooser = new \Janus\Model\Chooser($app);
-    return json_encode($chooser->getProxy('test'));
+    return json_encode($chooser->getProxy($service));
 });
 
 return $app;
