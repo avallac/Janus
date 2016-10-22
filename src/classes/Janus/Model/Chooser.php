88774002d72d037d2c51bdce19614e4d597cd2cd
Petr Petrenko <ppetrenko@xiag.ch>
2016-10-23 03:41:39 +0700

    смена коэфициентов

--- src/classes/Janus/Model/Chooser.php
+++ src/classes/Janus/Model/Chooser.php
@@ -36,7 +36,7 @@ class Chooser
             preg_match('/(\d+\.\d+\.\d+\.\d+)\:(\d+)/', $auth, $m);
             $sql = 'SELECT id FROM proxy WHERE hostname = ? AND port = ?';
             $status = $this->app['db']->fetchAll($sql, [$m[1], $m[2]]);
-            $sql = "UPDATE status SET statistic = (statistic*0.999 + ".$time."*0.001) WHERE proxy_id = ? AND service = ?";
+            $sql = "UPDATE status SET statistic = (statistic*0.99 + ".$time."*0.01) WHERE proxy_id = ? AND service = ?";
             $this->app['db']->executeQuery($sql, [$status[0]['id'], $service]);
             $this->app['db']->commit();
         } catch (\Exception $e) {
